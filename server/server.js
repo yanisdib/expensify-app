d10c8d2b79b1e5fc7828c64c28ea835e043382a2
Elhaym <elhaym@users.noreply.github.com>
2020-04-07 16:59:41 +0200

    Refactoring Header component, Adding Proxy and Express setup files, Removing useless files

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express(); // Create an express app
+
+const buildPath = path.join(__dirname, '..', 'build');
+const buildFile = path.join(__dirname, '..', 'build', 'index.html');
+
+app.use(express.static(buildPath)); // Tell Express to use the build directory to serve up all our static assets
+
+app.get('*', (req, res) => res.sendFile(buildFile)); // Send the user back to index.html if what he requested is not to be found 
+
+// Start Express server on port 8080
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server is up!')
+});
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => (
         <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
         <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Add an expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit an expense</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
