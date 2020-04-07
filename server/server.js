fb63f32c31b1bed0c8ea4b774263ca759a482bfd
Elhaym <elhaym@users.noreply.github.com>
2020-04-07 17:47:26 +0200

    Set up production build and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT;
 const app = express(); // Create an express app
 
 const buildPath = path.join(__dirname, '..', 'build');
@@ -10,6 +11,6 @@ app.use(express.static(buildPath)); // Tell Express to use the build directory t
 app.get('*', (req, res) => res.sendFile(buildFile)); // Send the user back to index.html if what he requested is not to be found 
 
 // Start Express server on port 8080
-app.listen(process.env.PORT || 8080, () => {
+app.listen(port || 8080, () => {
     console.log('Server is up!')
 });
