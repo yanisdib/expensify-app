6fe55077df89fd70dee6ea9c08fd21841df80edd
Elhaym <elhaym@users.noreply.github.com>
2020-04-07 17:57:26 +0200

    Set up production build and server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 const app = express(); // Create an express app
 
 const buildPath = path.join(__dirname, '..', 'build');
-const buildFile = path.join(__dirname, '..', 'build', 'index.html');
+const buildFile = path.join(__dirname, 'build', 'index.html');
 
 app.use(express.static(buildPath)); // Tell Express to use the build directory to serve up all our static assets
 
