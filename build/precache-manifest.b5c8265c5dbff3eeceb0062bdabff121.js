c1aba38807b5497abb19dde3565cdd410ae9dd66
Elhaym <elhaym@users.noreply.github.com>
2020-04-11 20:59:51 +0200

    Add Bootstrap 4

---
+++ build/precache-manifest.b5c8265c5dbff3eeceb0062bdabff121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e1200ebc7900976401541ff697be4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa20de270e120ae6c08",
+    "url": "/static/css/2.49a3f0e7.chunk.css"
+  },
+  {
+    "revision": "dfa20de270e120ae6c08",
+    "url": "/static/js/2.6508e0e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6508e0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5369555c25b690d7a295",
+    "url": "/static/js/main.5d16936a.chunk.js"
+  },
+  {
+    "revision": "1aa80bd2bd42a6fefab4",
+    "url": "/static/js/runtime-main.22a281fe.js"
+  }
+]);
