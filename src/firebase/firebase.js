814ede1f8e51a386cdb91c7cc9b6cefa1ccec72a
Elhaym <elhaym@users.noreply.github.com>
2020-04-18 19:41:54 +0200

    Correct and setup ENV files and variables for CRA

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,17 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-console.log(`Database url is: ${process.env.FIREBASE_API_KEY} & ENV is: ${process.env.NODE_ENV}`)
-
+console.log("NODE_ENV", process.env.NODE_ENV)
 firebase.initializeApp(config);
 
 const database = firebase.database();
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const webpack = require('webpack');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-    require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
-}
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.test' });
+};
 
 module.exports = {
     entry: './src/App.js',
